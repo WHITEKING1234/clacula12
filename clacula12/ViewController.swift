222ba3c3ce11771d675ad1425775750a54f1b1ff
bombag456 <bombag456@gmail.com>
2022-11-10 00:56:16 +0600

    dqdq

--- clacula12/ViewController.swift
+++ clacula12/ViewController.swift
@@ -8,6 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    
+    
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    
+    
+var result = 0
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+      result = Int(firstNumberTextField.text!)! + Int(secondNumberTextField.text!)!
+        resultLabel.text = String(result)
+        
+    }
+    
+    @IBAction func Umno(_ sender: UIButton) {
+        result = Int(firstNumberTextField.text!)! * Int(secondNumberTextField.text!)!
+        resultLabel.text = String(result)
+    }
+    
+    
+
+    @IBAction func delit(_ sender: UIButton) {
+        result = Int(firstNumberTextField.text!)! / Int(secondNumberTextField.text!)!
+        resultLabel.text = String(result)
+        
+    }
+    
+    
+    @IBAction func minusButtonTupped(_ sender: UIButton) {
+        result = Int(firstNumberTextField.text!)! - Int(secondNumberTextField.text!)!
+        resultLabel.text = String(result)
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
